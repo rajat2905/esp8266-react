5b7b94d481e98088f33a16d3f1873874c82c971c
Rajat Jain <rajat2905@gmail.com>
2023-08-09 09:18:28 +0530

    FINAL AQI BSEC ESP32

--- src/LightStateService.cpp
+++ src/LightStateService.cpp
@@ -1,18 +1,20 @@
 #include <LightStateService.h>
 #include <EEPROM.h>
 #include "bsec.h"
-#define STATE_SAVE_PERIOD UINT32_C(360 * 60 * 1000) // 360 minutes - 4 times a day
+#define STATE_SAVE_PERIOD UINT32_C(360 * 60 * 1000)  // 360 minutes - 4 times a day
 // Helper functions declarations
 void checkIaqSensorStatus(void);
 void errLeds(void);
 void loadState(void);
-void updateState(void);  
-String pubtopic="";
+void updateState(void);
+String pubtopic = "";
 // Create an object of the class Bsec
 Bsec iaqSensor;
 uint8_t bsecState[BSEC_MAX_STATE_BLOB_SIZE] = {0};
-uint16_t stateUpdateCounter = 0;                  
-uint16_t timeInterval=60000;
+uint16_t stateUpdateCounter = 0;
+uint16_t timeInterval = 10000;
+uint16_t lastmsg=0;
+bool SensorFaulty=false;
 String output;
 
 LightStateService::LightStateService(AsyncWebServer* server,
@@ -38,7 +40,7 @@ LightStateService::LightStateService(AsyncWebServer* server,
     _lightMqttSettingsService(lightMqttSettingsService) {
   // configure led to be output
   pinMode(LED_PIN, OUTPUT);
-  
+
   // configure MQTT callback
   _mqttClient->onConnect(std::bind(&LightStateService::registerConfig, this));
 
@@ -49,7 +51,7 @@ LightStateService::LightStateService(AsyncWebServer* server,
   addUpdateHandler([&](const String& originId) { onConfigUpdated(); }, false);
 }
 double round2(double value) {
-   return (int)(value * 100 + 0.5) / 100.0;
+  return (int)(value * 100 + 0.5) / 100.0;
 }
 const uint8_t bsec_config_iaq[] = {
 #include "config/generic_33v_3s_4d/bsec_iaq.txt"
@@ -58,59 +60,69 @@ void LightStateService::begin() {
   _state.ledOn = DEFAULT_LED_STATE;
   _state.swString = DEFAULT_SW_STRING;
 
-  //BSEC_setup
-EEPROM.begin(BSEC_MAX_STATE_BLOB_SIZE + 1); // 1st address for the length                              
-  Wire.begin(21,22);
-Serial.println("Starting");
-  //iaqSensor.begin(BME680_I2C_ADDR_PRIMARY, Wire);
+  // BSEC_setup
+  EEPROM.begin(BSEC_MAX_STATE_BLOB_SIZE + 1);  // 1st address for the length
+  Wire.begin(21, 22);
+  Serial.println("Starting");
+  // iaqSensor.begin(BME680_I2C_ADDR_PRIMARY, Wire);
   iaqSensor.begin(0x77, Wire);
-  output = "\nBSEC library version " + String(iaqSensor.version.major) + "." + String(iaqSensor.version.minor) + "." + String(iaqSensor.version.major_bugfix) + "." + String(iaqSensor.version.minor_bugfix);
+  output = "\nBSEC library version " + String(iaqSensor.version.major) + "." + String(iaqSensor.version.minor) + "." +
+           String(iaqSensor.version.major_bugfix) + "." + String(iaqSensor.version.minor_bugfix);
   Serial.println(output);
   checkIaqSensorStatus();
-  
+
   iaqSensor.setConfig(bsec_config_iaq);
   checkIaqSensorStatus();
 
-  loadState();                 
+  loadState();
   bsec_virtual_sensor_t sensorList[10] = {
-    BSEC_OUTPUT_RAW_TEMPERATURE,
-    BSEC_OUTPUT_RAW_PRESSURE,
-    BSEC_OUTPUT_RAW_HUMIDITY,
-    BSEC_OUTPUT_RAW_GAS,
-    BSEC_OUTPUT_IAQ,
-    BSEC_OUTPUT_STATIC_IAQ,
-    BSEC_OUTPUT_CO2_EQUIVALENT,
-    BSEC_OUTPUT_BREATH_VOC_EQUIVALENT,
-    BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_TEMPERATURE,
-    BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_HUMIDITY,
+      BSEC_OUTPUT_RAW_TEMPERATURE,
+      BSEC_OUTPUT_RAW_PRESSURE,
+      BSEC_OUTPUT_RAW_HUMIDITY,
+      BSEC_OUTPUT_RAW_GAS,
+      BSEC_OUTPUT_IAQ,
+      BSEC_OUTPUT_STATIC_IAQ,
+      BSEC_OUTPUT_CO2_EQUIVALENT,
+      BSEC_OUTPUT_BREATH_VOC_EQUIVALENT,
+      BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_TEMPERATURE,
+      BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_HUMIDITY,
   };
 
   iaqSensor.updateSubscription(sensorList, 10, BSEC_SAMPLE_RATE_LP);
   checkIaqSensorStatus();
 
   // Print the header
-  output = "Timestamp [ms], raw temperature [°C], pressure [hPa], raw relative humidity [%], gas [Ohm], IAQ, IAQ accuracy, temperature [°C], relative humidity [%], Static IAQ, CO2 equivalent, breath VOC equivalent";
+  output =
+      "Timestamp [ms], raw temperature [°C], pressure [hPa], raw relative humidity [%], gas [Ohm], IAQ, IAQ accuracy, "
+      "temperature [°C], relative humidity [%], Static IAQ, CO2 equivalent, breath VOC equivalent";
   Serial.println(output);
 }
 
 void LightStateService::loop() {
+   if (millis() - lastmsg > timeInterval) {
+    lastmsg=millis();
 
-if (iaqSensor.run()) { // If new data is available
-  unsigned long time_trigger = millis();
-    DynamicJsonDocument doc(1024);
-    doc["Time"]=time_trigger;
-    //doc["rawTemperature"]=round2(iaqSensor.rawTemperature);
-    doc["pressure"]=round2(iaqSensor.pressure);
-    //doc["rawHumidity"]=round2(iaqSensor.rawHumidity);
-    doc["gasResistance"]=round2(iaqSensor.gasResistance);
-    doc["iaq"]=round2(iaqSensor.iaq);
-    //doc["iaqAccuracy"]=iaqSensor.iaqAccuracy;
-    doc["temperature"]=round2(iaqSensor.temperature);
-    doc["humidity"]=round2(iaqSensor.humidity);
-    doc["staticIaq"]=round2(iaqSensor.staticIaq);
-    doc["co2"]=round2(iaqSensor.co2Equivalent);
-    doc["VOC"]=round2(iaqSensor.breathVocEquivalent);
-    //serializeJson(doc, Serial);
+  if (iaqSensor.run()) {  // If new data is available
+    unsigned long time_trigger = millis();
+      DynamicJsonDocument doc(1024);
+    doc["Time"] = time_trigger;
+    // doc["rawTemperature"]=round2(iaqSensor.rawTemperature);
+    doc["pressure"] = round2(iaqSensor.pressure);
+    // doc["rawHumidity"]=round2(iaqSensor.rawHumidity);
+    doc["gasResistance"] = round2(iaqSensor.gasResistance);
+    doc["iaq"] = round2(iaqSensor.iaq);
+    // doc["iaqAccuracy"]=iaqSensor.iaqAccuracy;
+    doc["temperature"] = round2(iaqSensor.temperature);
+    doc["humidity"] = round2(iaqSensor.humidity);
+    doc["staticIaq"] = round2(iaqSensor.staticIaq);
+    doc["co2"] = round2(iaqSensor.co2Equivalent);
+    doc["VOC"] = round2(iaqSensor.breathVocEquivalent);
+    if(SensorFaulty){
+       doc["deviceStatus"] = 0;
+    }else{
+       doc["deviceStatus"] = 1;
+    }
+    // serializeJson(doc, Serial);
     String payload;
     serializeJson(doc, payload);
     _mqttClient->publish(pubtopic.c_str(), 0, false, payload.c_str());
@@ -128,52 +140,58 @@ if (iaqSensor.run()) { // If new data is available
     output += ", " + String(iaqSensor.co2Equivalent);
     output += ", " + String(iaqSensor.breathVocEquivalent);
     Serial.println(output);
-  updateState();
+    updateState();
   } else {
     checkIaqSensorStatus();
   }
-
+  }
 }
 
 // Helper function definitions
-void checkIaqSensorStatus(void)
-{
+void checkIaqSensorStatus(void) {
   if (iaqSensor.bsecStatus != BSEC_OK) {
+    SensorFaulty=true;
     if (iaqSensor.bsecStatus < BSEC_OK) {
       output = "BSEC error code : " + String(iaqSensor.bsecStatus);
       Serial.println(output);
-      for (;;)
+
         errLeds(); /* Halt in case of failure */
+
     } else {
       output = "BSEC warning code : " + String(iaqSensor.bsecStatus);
       Serial.println(output);
     }
+  }else{
+    SensorFaulty=false;
   }
 
   if (iaqSensor.bme68xStatus != BME68X_OK) {
+    SensorFaulty=true;
     if (iaqSensor.bme68xStatus < BME68X_OK) {
       output = "BME68X error code : " + String(iaqSensor.bme68xStatus);
       Serial.println(output);
-      for (;;)
+
         errLeds(); /* Halt in case of failure */
+
     } else {
       output = "BME68X warning code : " + String(iaqSensor.bme68xStatus);
       Serial.println(output);
     }
+  }else{
+    SensorFaulty=false;
   }
 }
 
-void errLeds(void)
-{
+void errLeds(void) {
+  SensorFaulty=true;
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, HIGH);
-  Serial.println("Error..");
+  // Serial.println("Error..");
   delay(100);
   digitalWrite(LED_PIN, LOW);
   delay(100);
 }
-void loadState(void)
-{
+void loadState(void) {
   if (EEPROM.read(0) == BSEC_MAX_STATE_BLOB_SIZE) {
     // Existing state in EEPROM
     Serial.println("Reading state from EEPROM");
@@ -196,10 +214,10 @@ void loadState(void)
   }
 }
 
-void updateState(void)
-{
+void updateState(void) {
   bool update = false;
-  /* Set a trigger to save the state. Here, the state is saved every STATE_SAVE_PERIOD with the first state being saved once the algorithm achieves full calibration, i.e. iaqAccuracy = 3 */
+  /* Set a trigger to save the state. Here, the state is saved every STATE_SAVE_PERIOD with the first state being saved
+   * once the algorithm achieves full calibration, i.e. iaqAccuracy = 3 */
   if (stateUpdateCounter == 0) {
     if (iaqSensor.iaqAccuracy >= 3) {
       update = true;
@@ -219,7 +237,7 @@ void updateState(void)
 
     Serial.println("Writing state to EEPROM");
 
-    for (uint8_t i = 0; i < BSEC_MAX_STATE_BLOB_SIZE ; i++) {
+    for (uint8_t i = 0; i < BSEC_MAX_STATE_BLOB_SIZE; i++) {
       EEPROM.write(i + 1, bsecState[i]);
       Serial.println(bsecState[i], HEX);
     }
@@ -231,7 +249,9 @@ void updateState(void)
 
 void LightStateService::onConfigUpdated() {
   digitalWrite(LED_PIN, _state.ledOn ? LED_ON : LED_OFF);
-
+  timeInterval = atoi(_state.swString.c_str());
+  Serial.print("time interval set to ");
+  Serial.println(timeInterval);
 }
 
 void LightStateService::registerConfig() {
@@ -247,7 +267,7 @@ void LightStateService::registerConfig() {
     configTopic = settings.mqttPath + "/config";
     subTopic = settings.mqttPath + "/set";
     pubTopic = settings.mqttPath + "/state";
-    pubtopic=pubTopic;
+    pubtopic = pubTopic;
     doc["~"] = settings.mqttPath;
     doc["name"] = settings.name;
     doc["unique_id"] = settings.uniqueId;
